4879d7cc8378cf85921be81d437457277fb88817
windows 10 <dimpulsunayana205@gmail.com>
2023-02-22 23:30:51 +0530

    updated user repo

--- main.tf
+++ main.tf
@@ -31,11 +31,11 @@ resource "aws_vpc_peering_connection" "peer" {
   )
 }
 
-resource "aws_route" "default" {
-  route_table_id            = aws_vpc.main.default_route_table_id
-  destination_cidr_block    = "172.31.0.0/16"
-  vpc_peering_connection_id = aws_vpc_peering_connection.peer.peer_vpc_id
-}
+#resource "aws_route" "default" {
+#  route_table_id            = aws_vpc.main.default_route_table_id
+#  destination_cidr_block    = "172.31.0.0/16"
+#  vpc_peering_connection_id = aws_vpc_peering_connection.peer.peer_vpc_id
+#}
 
 resource "aws_route" "default1" {
   route_table_id            = data.aws_vpc.default_vpc.main_route_table_id
