6b07b7d3d8ef67a32a01e782ce9d68f7b1e7e768
windows 10 <dimpulsunayana205@gmail.com>
2023-02-23 21:43:26 +0530

    updated user repo

--- main.tf
+++ main.tf
@@ -60,6 +60,27 @@ resource "aws_vpc_peering_connection" "peer" {
 #  gateway_id                = aws_internet_gateway.igw.id
 #}
 
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  route {
+    cidr_block = data.aws_vpc.default_vpc.cidr_block
+    vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+  }
+
+
+  tags = merge(
+    local.common_tags,
+    { Name = "${var.env}-public-route_table" }
+  )
+}
+
+
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 
