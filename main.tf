1445e5cba2a6dde6258b5482cde12276dbd66961
windows 10 <dimpulsunayana205@gmail.com>
2023-02-24 15:03:08 +0530

    updated user repo

--- main.tf
+++ main.tf
@@ -147,10 +147,9 @@ data "aws_ami" "example" {
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.example.id
-  instance_type = "t3.medium"
+  instance_type = "t3.small"
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
   subnet_id   = aws_subnet.private_subnet.*.id[0]
-  zone         = "us-east-1a"
 
   tags = {
     Name = "test-centos8"
